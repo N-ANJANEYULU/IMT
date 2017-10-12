54877dd3e2f283c574dd1bc44bf8466e8c646e04
Nallapaneni A <Nallapaneni39@gmail.com>
2017-10-12 08:42:07 +0530

    password changes

--- IMT/src/main/java/com/acc/controller/UserController.java
+++ IMT/src/main/java/com/acc/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.acc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,18 +8,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.acc.model.UserRegister;
+import com.acc.services.DataServices;
 @Controller
 public class UserController {
+	
+	
+	@Autowired
+	DataServices dataServices;
+
+	public DataServices getDataServices() {
+		return dataServices;
+	}
+	public void setDataServices(DataServices dataServices) {
+		this.dataServices = dataServices;
+	}
+	
+	
 	@RequestMapping(value="/urget")
 	public String showUserRegistrationPage(@ModelAttribute("userRegistration")UserRegister userRegistration,ModelMap m){
 		m.addAttribute("msg","i am from UserRegister Controller");
-		System.out.println( userRegistration.getFirstName());
+		//System.out.println( userRegistration.getFirstName());
+		
 		return "UserRegistration";
 	}
      @RequestMapping(value="/urpost",method=RequestMethod.POST)
      public String postSubmit(@ModelAttribute("userRegistration")UserRegister userRegistration,ModelMap m){
     	 m.addAttribute("msg","i am from UserRegister Controller");
- 		System.out.println( userRegistration.getFirstName());
+ 		try {
+			dataServices.addEntity(userRegistration);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
  		return "UserRegistration";
      }
 }
